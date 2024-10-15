075b62bce9c01cc512969a92ca891c4fa2bae4b4
sama888-i <samaki-bp215@code.edu.az>
2024-10-15 12:55:53 +0400

    first commit

---
+++ task 1.cs
@@ -0,0 +1,98 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+             car car1 = new car("Bmw" ,"i5" ,4);
+             car1.mile = 295;
+             car1.ShowFullInfo();
+             bicycle bicycle1 = new bicycle("Altec","Sirius" ,2);
+             bicycle1.mile = 65;
+             bicycle1.ShowFullInfo();
+            /*car[] cars = new car[1];
+            cars[0] = new car("Ford", "Mustang", 4);
+            
+            Array.Resize(ref cars, 2);
+            cars[1] = new car("Bmw", "i5", 4);
+            foreach (car car in cars)
+            {
+                car.ShowFullInfo();
+
+            }
+            bicycle[] bicycles = new bicycle[1];
+            bicycles[0] = new bicycle("asd","dsa",2);
+            Array.Resize(ref bicycles, 2);
+            bicycles[1] = new bicycle("Altec","Sirius",2);
+            foreach (bicycle bicycle in bicycles )
+            {
+
+                bicycle.ShowFullInfo();
+            }
+            */
+
+
+              
+        }
+        public class car : vehicle
+        {
+            public int doorsNum;
+            public car(string brand, string model, int doorsNum) : base(brand, model)
+            {
+                this.doorsNum = doorsNum;
+
+
+            }
+            public void ShowFullInfo()
+            {
+
+                Console.WriteLine($"brand:{brand}, model:{model},mile :{mile},doorsNum:{doorsNum}");
+            }
+           
+            
+
+        }
+        public class bicycle:vehicle
+        {
+            public int whellNum;
+            public bicycle (string brand, string model, int whellNum ):base(brand, model)
+            {
+                this.whellNum = whellNum;
+
+
+            }
+            public void ShowFullInfo()
+            {
+                Console.WriteLine($"brand:{brand},model:{model},mile:{mile},whellNum:{whellNum}");
+
+            }
+
+
+
+        }
+        public class vehicle
+        {
+            public string brand;
+            public string model;
+            public double mile;
+            public vehicle( string brand, string model )
+            {
+                this.brand = brand;
+                this.model = model;
+                
+
+            }
+            public vehicle(string brand, string model, double mile)
+            {
+                this.mile = mile;
+            
+            
+            }
+
+
+
+        }
+    }
+}
---
+++ task 2.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Asus",8,2000.88,16,512);
+            notebook.Sale();
+            notebook.ShowFullData();
+
+        }
+        public class Notebook : Product
+        {
+            public byte ram;
+            public int storage;
+            public Notebook(string name, int count, double price, byte ram, int storage) : base(name, count, price)
+            {
+                this.ram = ram;
+                this.storage = storage;
+            }
+            public void GetFullInfo()
+            {
+                Console.WriteLine($"name:{name},count:{count},price:{price}, ram:{ram},storage:{storage},IsStock:{IsStock}");
+
+
+            }
+            public void ShowFullData()
+            {
+                if (IsStock )
+                {
+                    Console.WriteLine(GetFullInfo());
+
+                }
+                else
+                {
+                    Console.WriteLine("Mehsul Yoxdur!");
+                }
+            }
+
+
+
+        }
+        public class Product
+        {
+            public string name;
+            public string description;
+            public int count;
+            public bool IsStock;
+            public double price;
+            public Product(string name, int count, double price)
+            {
+                this.name = name;
+                this.count = count;
+                this.price = price;
+
+
+
+
+            }
+            public Product(string name, int count, double price, string description, bool IsStock)
+            {
+                this.description = description;
+                this.IsStock = IsStock;
+
+            }
+            public void Sale()
+            {
+                if (IsStock && count >0)
+                {
+                    Console.WriteLine("Mehsul Satisdadir");
+
+                }
+                else
+                {
+                    Console.WriteLine("Mehsul Yoxdur!");
+                }
+
+            }
+
+        }
+    }
+}
